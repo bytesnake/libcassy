c158da160795b363d8251e7ee5d49763ba7e063a
orbital1337 <t_troebs@cojobo.net>
2013-03-12 17:38:21 +0100

    Slight changes to oscillotest.c

--- test/oscilloscope/oscillotest.c
+++ test/oscilloscope/oscillotest.c
@@ -6,31 +6,38 @@ int main( int argc, char **argv )
 {
 	ca_handle_t handle;
 	ca_cassy_t cassy;
+	ca_ostatus2_t ostatus;
 	ca_oarray_t oarray;
-	int i;
+	int interval, values, i;
 
-	if ( argc < 2 )
+	if ( argc < 4 )
 	{
-		printf( "Usage: oscillotest /dev/node\n" );
+		printf( "Usage: oscillotest /dev/node interval values\n" );
 		return 0;
 	}
+
+	interval = atoi( argv[2] );
+	values = atoi( argv[3] );
 
 	handle = CA_GetDeviceHandle( argv[1] );
 	cassy = CA_OpenCassy( handle, CA_VERSION_SENSORCASSY2, 1 );
 
 	CA_GetInputValueA( cassy, CA_RANGE_10V );
 
-	CA_ResetOscilloscope2( cassy, 100, 10, 0, 10000, CA_OMASK_4MMSOCKA, CA_OTRIG_IMMEDIATE, 0 );
+	CA_ResetOscilloscope2( cassy, interval, 10, 0, values, CA_OMASK_4MMSOCKA, CA_OTRIG_IMMEDIATE, 0 );
 	CA_StartOscilloscope( cassy );
 
-	usleep( 1000 * 1250 ); // should be done after 1.25 seconds
+	do {
+		ostatus = CA_GetOscilloscopeStatus2( cassy );
+		usleep( 1000 * 500 );
+	} while ( ostatus.status != CA_OSTATUS_STOPPED );
 
-	oarray = CA_GetOscilloscopeArray2A( cassy, CA_RANGE_10V, 0, 10000 );
+	oarray = CA_GetOscilloscopeArray2A( cassy, CA_RANGE_10V, 0, interval );
 
 	printf( "# Oscilloscope data from %s - status %i\n", argv[1], oarray.status );
 
 	for ( i = 0; i < oarray.length; i++ )
-		printf( "%i\t%f\n", i, oarray.values[i] );
+		printf( "%f\t%f\n", i * interval * 0.000001, oarray.values[i] );
 
 	CA_FreeOscilloscopeArray( &oarray );
 
