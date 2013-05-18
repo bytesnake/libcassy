f56389419a857183a187e6bed7f8c061a3b63d59
orbital1337 <t_troebs@cojobo.net>
2013-05-18 15:52:11 +0200

    Fixed kernel driver detach

--- source/ca_inout_libusb.c
+++ source/ca_inout_libusb.c
@@ -77,8 +77,6 @@ ca_handle_t CA_GetDeviceHandle( const char *desc )
 	else if ( libusb_kernel_driver_active( handle, 0 )
 			&& libusb_detach_kernel_driver( handle, 0 ) != 0 )
 		CA_SetLastError( CA_ERROR_IO_OPEN );
-	else if ( libusb_detach_kernel_driver( handle, 0 ) != 0 )
-		CA_SetLastError( CA_ERROR_IO_OPEN );
 	else if ( libusb_claim_interface( handle, 0 ) != 0 )
 		CA_SetLastError( CA_ERROR_IO_OPEN );
 
