3797cf0f7e2aeea8e66aacfc2776124d3e47379d
orbital1337 <t_troebs@cojobo.net>
2013-05-18 15:49:40 +0200

    Serial support

--- source/ca_inout_devnodes.c
+++ source/ca_inout_devnodes.c
@@ -10,16 +10,59 @@ void CA_Deinit()
 	CA_ResetError();
 }
 
+struct termios *CA_PrepareSerialDevice( int filedesc )
+{
+	struct termios settings;
+	struct termios *settingsb;
+
+	CA_ResetError();
+	
+	settingsb = (struct termios *) malloc( sizeof (struct termios) );
+	
+	if ( tcgetattr( filedesc, settingsb ) < 0 )
+	{
+		CA_SetLastError( CA_ERROR_IO_OPEN );
+		return NULL;
+	}
+
+	settings = *settingsb;
+
+	cfsetospeed( &settings, B38400 );
+	cfsetispeed( &settings, B38400 );
+	cfmakeraw( &settings );
+
+	settings.c_cc[VMIN] = 8;
+	settings.c_cc[VTIME] = 0;
+
+	settings.c_cflag &= ~(CSTOPB | CRTSCTS);
+	settings.c_cflag |= CLOCAL | CREAD;
+
+	if ( tcsetattr( filedesc, TCSANOW, &settings ) < 0 )
+		CA_SetLastError( CA_ERROR_IO_OPEN );
+
+	return settingsb;
+}
+
 ca_handle_t CA_GetDeviceHandle( const char *desc )
 {
+	struct serial_struct serinfo;
 	ca_handle_t handle;
 
 	CA_ResetError();
-
-	handle = open( desc, O_RDWR );
-
-	if ( handle == -1 )
+	
+	handle = (ca_handle_t) malloc( sizeof *handle );
+	handle->filedesc = open( desc, O_RDWR | O_NOCTTY );
+	
+	if ( handle->filedesc == -1 )
+	{
 		CA_SetLastError( CA_ERROR_IO_OPEN );
+		return NULL;
+	}
+	
+	if ( ioctl( handle->filedesc, TIOCGSERIAL, &serinfo ) < 0 )
+		handle->settingsb = NULL;
+	else
+		handle->settingsb = CA_PrepareSerialDevice( handle->filedesc );
 
 	return handle;
 }
@@ -30,8 +73,9 @@ void CA_CloseDeviceHandle( ca_handle_t handle )
 
 	CA_ResetError();
 
-	result = close( handle );
-
+	result = close( handle->filedesc );
+	free( handle );
+	
 	if ( result != 0 )
 		CA_SetLastError( CA_ERROR_IO_CLOSE );
 }
@@ -56,9 +100,9 @@ ca_cassy_t CA_OpenCassy( ca_handle_t handle, ca_version_t expected, int id )
 
 	cassy.handle = handle;
 	cassy.id = id;
-	cassy.version = expected;
-
-	// cassy.version = CA_GetHardwareVersion( cassy ) >> 8;
+	
+	cassy.version = handle->settingsb != NULL ? CA_VERSION_SENSORCASSY : CA_VERSION_SENSORCASSY2;
+	cassy.version = CA_GetHardwareVersion( cassy ) >> 8;
 
 	return cassy;
 }
@@ -69,7 +113,7 @@ void CA_SendData( ca_handle_t handle, uint8_t *data, int length )
 
 	CA_ResetError();
 
-	trans = write( handle, data, length );
+	trans = write( handle->filedesc, data, length );
 
 	if ( trans != length )
 		CA_SetLastError( CA_ERROR_IO_WRITE );
@@ -81,7 +125,7 @@ void CA_RecvData( ca_handle_t handle, uint8_t *data, int length )
 
 	CA_ResetError();
 
-	trans = read( handle, data, length );
+	trans = read( handle->filedesc, data, length );
 
 	if ( trans != length )
 		CA_SetLastError( CA_ERROR_IO_READ );
